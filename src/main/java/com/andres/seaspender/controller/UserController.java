389b04128dcd4f99a69a88c8e5b5fb8d6567fc03
andresdiazv <adiaz616@fiu.edu>
2023-05-01 12:31:52 -0400

    can retreive all users from databse postman

--- src/main/java/com/andres/seaspender/controller/UserController.java
+++ src/main/java/com/andres/seaspender/controller/UserController.java
@@ -4,9 +4,12 @@ package com.andres.seaspender.controller;
 import com.andres.seaspender.model.User;
 import com.andres.seaspender.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UserController {
@@ -14,8 +17,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    @PostMapping("/users")
+    @PostMapping("/user")
     User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping("/users")
+    List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
